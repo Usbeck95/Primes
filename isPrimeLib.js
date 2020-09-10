584893ebd63496da20b39347b2aa5c053c2850e7
Carina <carinausbeck@live.dk>
2020-09-10 21:33:13 +0200

    tredjer commit

--- isPrimeLib.js
+++ isPrimeLib.js
@@ -11,10 +11,14 @@ const isPrime = function (arg) {
 
 
 const isPrime = function (arg) {
-    for(let i = 3; i < arg; i += 2) {
+    for(let i = 3; i < arg; i += 1) {
         if (arg % i === 0) {
             return false;
         }
     }
  return true;
 }
+// let x = 6*i-1
+// let y = 6*i+1 
+// når man når kvadratroden kan man holde op med at dividere
+// && Math.sqrt(arg) === 0
